48531b08b29c1a50cb708f94188bd20025d7d023
HuzefaEssaji <essajihuzefa21@gmail.com>
2020-03-24 21:18:55 +0530

    added two file

--- csvdata.py
+++ csvdata.py
@@ -3,7 +3,7 @@ import pandas as pd
 colnames = ['Beverage', 'Snack', 'MainCourse', 'Other']
 data = pd.read_csv('dishes.csv', names=colnames)
 
-
+# this is also random comment
 Beverages = data.Beverage.dropna(axis=0, how='any').tolist()
 Snacks = data.Snack.dropna(axis=0, how='any').tolist()
 MainCourses = data.MainCourse.dropna(axis=0, how='any').tolist()
