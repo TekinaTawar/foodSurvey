84b84b2a5784b8a24615a0a394baf1df01cad7cd
TekinaTawar <astroaniket1@gmail.com>
2020-03-28 23:25:26 +0530

    added more things to todo .c and  done email auth

--- todo.c
+++ todo.c
@@ -1,9 +1,22 @@
-//todo: 1. Change background color to blue
-//todo: 2. Add back button.
-//todo: 3. Change font.
-//todo: 4. add more padding in the profile form
-//todo: 5. get all the buttons outside.
-//todo: 6. Add * Required to html page
-//todo: 7. add a progress bar 
+//todo: . Change background color to blue
+//todo: . Change font.
+
+//todo: . Add back button.
+//todo: . add more padding in the profile form
+//todo: . get all the buttons outside.
+//todo: . Add * Required to html page
+//todo: . add a progress bar 
+//todo: . change first page to start
+//todo: . button pink
+
+//? page scroll up in some cases as in age as it is down keyboard.
+
+// * Done
+// * email authentication
+
+
+
+
 
 // https://docs.google.com/forms/d/1BljXrLkWkhhUXX53b15tNYq1Lp7nO_NhhiLl8YvD5ko/edit
+// http://tekinatawar.pythonanywhere.com/
