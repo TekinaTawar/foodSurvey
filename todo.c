29600310eff08be179b0ec97da79165485858e53
TekinaTawar <astroaniket1@gmail.com>
2020-03-29 00:02:00 +0530

    changed todo.c

--- todo.c
+++ todo.c
@@ -1,13 +1,15 @@
+// !Tekina
 //todo:  Change background color to blue
 //todo:  Change font.
 
+// !Huzefa
+//todo:  Add * Required to html page
 
 //todo:  add more padding in the profile form
 //todo:  Add back button.
 //todo:  get all the buttons outside.
-//todo:  Add * Required to html page
 //todo:  add a progress bar 
-//todo:  button pink
+//todo:  in mobile buttons remain pink after tap solve it
 //todo:  make width smaller or container
 
 //? page scroll up in some cases as in age as it is down keyboard.
