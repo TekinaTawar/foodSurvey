f772c52fa5aafa91197bb808d66ea0df06b8a72e
TekinaTawar <astroaniket1@gmail.com>
2020-03-28 22:16:22 +0530

    added todo.c

---
+++ todo.c
@@ -0,0 +1,9 @@
+//todo: 1. Change background color to blue
+//todo: 2. Add back button.
+//todo: 3. Change font.
+//todo: 4. add more padding in the profile form
+//todo: 5. get all the buttons outside.
+//todo: 6. Add * Required to html page
+//todo: 7. add a progress bar 
+
+// https://docs.google.com/forms/d/1BljXrLkWkhhUXX53b15tNYq1Lp7nO_NhhiLl8YvD5ko/edit
