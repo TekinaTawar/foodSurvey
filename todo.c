c73423b676c652c4c8a01b526a67d096f4253b8f
TekinaTawar <astroaniket1@gmail.com>
2020-03-29 14:18:35 +0530

    feature done

--- todo.c
+++ todo.c
@@ -1,20 +1,22 @@
+//download extension called "better comments "to see this file properly.
+
 // !Tekina
-//todo:  Change background color to blue
+//todo:  add a progress bar 
+//todo:  in mobile buttons remain pink after tap solve it
 
 // !Huzefa
 //todo:  Add * Required to html page
-
-//todo:  add more padding in the profile form
 //todo:  Add back button.
 //todo:  get all the buttons outside.
-//todo:  add a progress bar 
-//todo:  in mobile buttons remain pink after tap solve it
-//todo:  make width smaller or container
+//todo:  add paddng to all the cards.
+//todo: for some reason snacks is comming big chage it back
 
 //? page scroll up in some cases as in age as it is down keyboard.
 
+// * add more padding in the profile form
 // * Done
 // * Change font.
+// * Change background color to blue
 // * change first page to start
 // * email authentication
 
