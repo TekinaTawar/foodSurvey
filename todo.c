2bc47ce29494d18cb672019eeda3fe68fae6abcc
TekinaTawar <astroaniket1@gmail.com>
2020-03-30 09:24:32 +0530

    Q sequence, submit to next, back workng, question no, font size.

--- todo.c
+++ todo.c
@@ -1,17 +1,24 @@
 //download extension called "better comments "to see this file properly.
+// !time taking task.
+//todo: email authentication in backend (to check weather such email exist or not).
+//todo:  add a progress bar 
 
 // !Tekina
-//todo:  add a progress bar 
 
 // !Huzefa
 
-
-
-
 // * Done
 
-// *  in mobile buttons remain pink after tap solve it
+// * V0.0.3+
+// * font size of heading should be larger and everything should be small
+// * question number.
+// * back not working 
+    //? In case of back of pages the data is lost.
+// * change submit to next
+// * question sequence wrong
+// * in mobile buttons remain pink after tap solve it
 // * page scroll up in some cases as in age as it is down keyboard.
+// * V0.0.3
 // * get all the buttons outside.
 // * for some reason snacks is comming big chage it back
 // * Add back button.
