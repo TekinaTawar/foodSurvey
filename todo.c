56e731488749431c8fc50de1dd2a81d599f43e62
TekinaTawar <astroaniket1@gmail.com>
2020-03-30 16:27:01 +0530

    made a lot of changes

--- todo.c
+++ todo.c
@@ -3,15 +3,24 @@
 //todo: email authentication in backend (to check weather such email exist or not).
 //todo:  add a progress bar 
 //todo: button late loading problem (change css to min.css)
+//todo: In case of back of pages the data is lost.
 
 // !Tekina
+
 
 // !Huzefa
 
 
 // * Done
-
-// * v0.1.2
+// * V0.1.3
+//todo: last page shadow
+//todo: tell us a bit about yourself to you.
+//todo: about us jo likha hai uska font size and please leave your... ka font size same hoga to about us ka ek do point badha dena
+//todo: remove thank you image.
+//todo: thankyou text align center.
+//todo: only beverage in first page small
+//todo: next button first page outside.
+// * V0.1.2
 // * browser back button disabled on last page.
 // * V0.1.1
 // * find a an image for ico.
@@ -29,7 +38,6 @@
 // * font size of heading should be larger and everything should be small
 // * question number.
 // * back not working 
-    //? In case of back of pages the data is lost.
 // * change submit to next
 // * question sequence wrong
 // * in mobile buttons remain pink after tap solve it
