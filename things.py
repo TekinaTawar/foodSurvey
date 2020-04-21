7cdb2664c318dfa46edee421e46e990a17446960
TekinaTawar <astroaniket1@gmail.com>
2020-04-21 08:53:31 +0530

    removed email already used message,removed test.db, things.py.

--- things.py
+++
@@ -1,8 +0,0 @@
-# # {% if message %}
-# #     <p class ="message">{{message | safe }}</p>
-# # {% endif %}
-
-
-# if db.session.query(FoodSurvey).filter(FoodSurvey.email == user_email).count() == 0:
-# you can pass message in render_tmeplate
-#     render_template('index', message='You have already submitted feedback')
